c69c96625a3eda545d24a7a5260636eaba7bd533
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-25 14:42:36 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,7 @@
 class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
+  ::GENREs = ["Thriller", "Science Fiction", "Romance"]
 
   def initialize(title)
     @title = title
